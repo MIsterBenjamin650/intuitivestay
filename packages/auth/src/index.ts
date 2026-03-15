b5ab33715098790fc30bfb6419f2f275a2eaffff
Ben Thomson <hello@ben.pw>
2026-03-15 18:08:33 +0000

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,34 @@
+import { expo } from "@better-auth/expo";
+import { db } from "@intuitive-stay/db";
+import * as schema from "@intuitive-stay/db/schema/auth";
+import { env } from "@intuitive-stay/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [
+    env.CORS_ORIGIN,
+    "intuitive-stay://",
+    ...(env.NODE_ENV === "development"
+      ? ["exp://", "exp://**", "exp://192.168.*.*:*/**", "http://localhost:8081"]
+      : []),
+  ],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [expo()],
+});
