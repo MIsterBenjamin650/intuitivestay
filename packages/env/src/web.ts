fd827db27bbdf4862bb23ca3735f0fe77e38bcbe
MIsterBenjamin650 <misterbenjaminsmith@outlook.com>
2026-04-06 18:35:25 +0700

    feat: add Stripe and Wix plan env vars

--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -6,6 +6,10 @@ export const env = createEnv({
   client: {
     VITE_SERVER_URL: z.url(),
     VITE_AUTH_URL: z.url(),
+    VITE_WIX_PLAN_URL_HOST: z.url(),
+    VITE_WIX_PLAN_URL_PARTNER: z.url(),
+    VITE_WIX_PLAN_URL_FOUNDER: z.url(),
+    VITE_WIX_BILLING_URL: z.url(),
   },
   runtimeEnv: (import.meta as any).env,
   emptyStringAsUndefined: true,
